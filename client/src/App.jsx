2133a41913d5f28def58b477856e65cd36d794eb
Jaminton Moreno <jaminton.morenof@gmail.com>
2024-04-27 11:06:29 -0500

    Create and configuration react project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { Taskspage } from "/pages/Taskspage";
+import { TaskFormPage } from "/pages/TaskFormPage";
+import { Navigation } from './components/Navigation';
+
+
+
 
 const App = () => {
   return (
-    <div>Hello world</div>
-  )
+    <BrowserRouter>
+      <Navigation/>    
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/tasks" />} />
+        <Route path="/tasks" element={<Taskspage />} />
+        <Route path="/tasks-create" element={<TaskFormPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ client/src/components/TaskList.jsx
@@ -0,0 +1,14 @@
+import {useEffect} from 'react'
+
+
+export function TaskList() {
+
+    useEffect(() => {
+      console.log('Pagina cargada')    
+    }, [])
+    
+
+  return (
+    <div>TaskList</div>
+  )
+}
