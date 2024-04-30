f2870f1c89f014fbb9f73d7fd3663d0ce7d35ddc
Jaminton Moreno <jaminton.morenof@gmail.com>
2024-04-30 09:12:54 -0500

    Create pages tasks

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import {useForm} from 'react-hook-form'
+
 
 export function TaskFormPage() {
+
+  const {register, handleSubmit, formState:{
+    errors
+  }} = useForm()
+
+  const onSubmit = handleSubmit(data => {
+      console.log(data)
+  })
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Title" 
+        {...register("title", {required: true})}
+        />
+        {errors.title && <span>Title is required</span>}
+        <textarea rows="3" placeholder='Description'
+        {
+           ...register("description", {required: true})
+        }></textarea>
+        {errors.description && <span>Description is required</span>}
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
